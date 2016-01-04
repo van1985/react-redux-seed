7009e353c3404114928959bba4fe7d860ed00e27
leandro.garcia <leandro.garcia@globant.com>
2016-01-04 17:47:59 -0300

    refactor UT

--- src/test/counter.spec.js
+++ src/test/counter.spec.js
@@ -22,24 +22,45 @@ describe('Counter', ()=>{
     expect(actual).toIncludeJSX(expected);
   });
 
-  it ('should show the counter as inactive - class', ()=>{
-    const renderer = TestUtils.createRenderer();
-    renderer.render(<Counter isActive={false} />);
-    const actual = renderer.getRenderOutput()
-      .props.className.includes('inactive counter');
-    console.log(actual);
-    const expected =true;
-    expect(actual).toEqual(expected);
-  });
+  describe('isActive', ()=>{
 
+    function renderCounter(isActive,className){
+      const renderer = TestUtils.createRenderer();
+      renderer.render(<Counter isActive={isActive} />);
+      return renderer.getRenderOutput()
+        .props.className.includes(className+' counter');
+    }
 
-  it ('should show the counter as active - class', ()=>{
-    const renderer = TestUtils.createRenderer();
-    renderer.render(<Counter isActive={true} />);
-    const actual = renderer.getRenderOutput()
-      .props.className.includes('active counter');
-    const expected =true;
-    expect(actual).toEqual(expected);
+    /**
+    * Check if the class is inactive
+    **/
+    it ('should show the counter as inactive - class', ()=>{
+      /*
+      const renderer = TestUtils.createRenderer();
+      renderer.render(<Counter isActive={false} />);
+      const actual = renderer.getRenderOutput()
+        .props.className.includes('inactive counter');
+      console.log(actual);
+      const expected =true;
+      */
+
+      expect(renderCounter(false,'inactive')).toEqual(true);
+    });
+
+    /**
+    * Check if the class is active
+    **/
+    it ('should show the counter as active - class', ()=>{
+      /*
+      const renderer = TestUtils.createRenderer();
+      renderer.render(<Counter isActive={true} />);
+      const actual = renderer.getRenderOutput()
+        .props.className.includes('active counter');
+      const expected =true;
+      */
+
+      expect(renderCounter(true,'active')).toEqual(true);
+    });
   });
 
 });
